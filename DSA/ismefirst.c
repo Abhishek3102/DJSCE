ba2617b73f7f573ed1629cf3746716065e500281
Abhishe16 <ankushpan694@gmail.com>
2024-07-15 02:28:59 +0530

    added files

---
+++ DSA/ismefirst.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main(){
+    int m,n;
+    printf("Enter two numbers:\n");
+    scanf("%d %d",&m,&n);
+    printf("The addition of two numbers is: %d",m+n);
+}
